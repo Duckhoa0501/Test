32be85cb1695930841ccc7d589ab1f65c7962806
Duckhoa0501 <150015062+Duckhoa0501@users.noreply.github.com>
2024-11-26 19:56:14 +0700

    Add files via upload

---
+++ báo cáo/Click jvs/click.js
@@ -0,0 +1,9 @@
+/* Bạn hãy code jvs theo yêu cầu sau:
+- Khi ấn vô biểu thượng menu (open) 3 sọc ngang thì khối màu trắng giới 
+thiệu bản thân sẽ hiện ra.
+- Khi thẻ hiện ra rồi thì ta ấn biểu tượng close thì khối màu trắng giới 
+thiệu bản thân sẽ ẩn đi.*/
+// ========================Code bên dưới này================================
+
+
+
---
+++ báo cáo/Hiệu đếm số jvs/index.js
@@ -0,0 +1,8 @@
+/* code javas sao cho khi ấn vào đếm thì số 0 tăng dần lền 1,2,3,4,5....... */
+// Sinh viên Code ở dưới đây
+
+
+
+
+
+
